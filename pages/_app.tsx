7117ca9e1b9bb5ac4d4a39d1cec330fa81256075
Hosein <a.h.pouyanmehr@gmail.com>
2021-11-25 16:35:18 +0330

    Install Roboto Font and add import it in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 // type
 import type { AppProps } from "next/app";
 // style
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
