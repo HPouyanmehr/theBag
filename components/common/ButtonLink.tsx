679d0f37a7ab87a8a691120ae4ed3a6ddda7d646
Hosein <a.h.pouyanmehr@gmail.com>
2021-11-29 16:24:49 +0330

    Add CustomButton Component

--- components/common/ButtonLink.tsx
+++ components/common/ButtonLink.tsx
@@ -2,29 +2,24 @@
 import * as React from 'react';
 // next
 import Link from 'next/link';
-// @mui
-import { Button, ButtonProps, styled } from '@mui/material';
+// custom component
+import CustomButton from 'components/common/CustomButton';
 // type
+import { ButtonProps } from '@mui/material';
 import { Property } from 'csstype';
 interface ButtonLinkProps extends ButtonProps {
   href?: string;
   margin?: Property.Margin;
 }
 
-const CustomButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  textTransform: 'capitalize',
-  boxShadow: 'none',
-  '&: hover': {
-    boxShadow: 'none',
-  },
-}));
-
 const ButtonLink: React.FunctionComponent<ButtonLinkProps> = (props) => {
   const { children, href = '#', margin, ...otherProps } = props;
   return (
     <Link href={href}>
       <a style={{ margin }}>
-        <CustomButton {...otherProps}>{children}</CustomButton>
+        <CustomButton disableRipple {...otherProps}>
+          {children}
+        </CustomButton>
       </a>
     </Link>
   );
---
+++ components/common/CustomButton.tsx
@@ -0,0 +1,26 @@
+// react
+import * as React from 'react';
+// @mui
+import { Button, ButtonProps, styled } from '@mui/material';
+// type
+interface CustomButtonProps {}
+
+const CustomButtonRoot = styled(Button)<ButtonProps>(({ theme }) => ({
+  textTransform: 'capitalize',
+  boxShadow: 'none',
+  '&: hover': {
+    boxShadow: 'none',
+    background: 'none',
+  },
+  '&: active': {
+    opacity: 0.5,
+    transition: '0.3s ease-in-out',
+  },
+}));
+
+const CustomButton: React.FunctionComponent<CustomButtonProps> = (props) => {
+  const { children, ...otherProps } = props;
+  return <CustomButtonRoot {...otherProps}>{children}</CustomButtonRoot>;
+};
+
+export default CustomButton;
