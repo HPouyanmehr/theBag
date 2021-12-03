5634503ae0694c9ae480fdf0bbd82b4ff03f03b1
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-03 15:33:36 +0330

    Import swiperjs styles in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import 'styles/globals.css';
 import lightTheme from 'styles/theme/lightTheme';
 // type
