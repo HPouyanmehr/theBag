0d621020e0972e95fd7af015edac94f7460127f7
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-03 20:17:13 +0330

    Customize RecentProjects section swiper and add title for the section

--- components/section/RecentProjects.tsx
+++ components/section/RecentProjects.tsx
@@ -5,6 +5,8 @@ import { Pagination, Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // @mui
 import { Card, CardContent, Typography } from '@mui/material';
+// style
+import styles from 'styles/components/RecentProjects.module.scss';
 // type
 interface RecentProjectsProps {}
 
@@ -36,45 +38,52 @@ const RecentProjects: React.FunctionComponent<RecentProjectsProps> = (
 
   return (
     <>
-      <Swiper
-        centeredSlides
-        spaceBetween={50}
-        slidesPerView={3}
-        modules={[Navigation, Pagination]}
-        navigation
-        pagination={{
-          dynamicBullets: true,
-          clickable: true,
-        }}
-        onSwiper={(swiper) => setActiveIndex(swiper.activeIndex)}
-        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-        style={{
-          margin: '2rem',
-          paddingBottom: '2rem',
-          paddingTop: '2rem',
-          minHeight: '6rem',
-        }}
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide
-            key={slide.title + index}
-            style={{
-              transform: index === activeIndex ? 'scale(1)' : 'scale(0.75)',
-              transition: '0.2s all ease-in-out',
-            }}
-          >
-            <Card>
-              <CardContent>
-                <Typography component="p" textAlign="center">
-                  {`${index} - ${
-                    index === activeIndex ? 'active' : 'disabled'
-                  }`}
-                </Typography>
-              </CardContent>
-            </Card>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={styles.sliderWrapper}>
+        <Typography
+          component="h2"
+          variant="h4"
+          color="primary"
+          textAlign="center"
+        >
+          My Recent Projects
+        </Typography>
+        <Swiper
+          centeredSlides
+          spaceBetween={50}
+          slidesPerView={3}
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{
+            dynamicBullets: true,
+            clickable: true,
+          }}
+          onSwiper={(swiper) => setActiveIndex(swiper.activeIndex)}
+          onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+          style={{
+            margin: '2rem',
+          }}
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide
+              key={slide.title + index}
+              style={{
+                transform: index === activeIndex ? 'scale(1)' : 'scale(0.75)',
+                transition: '0.2s all ease-in-out',
+              }}
+            >
+              <Card sx={{ height: '10rem' }}>
+                <CardContent>
+                  <Typography component="p" textAlign="center">
+                    {`${index} - ${
+                      index === activeIndex ? 'active' : 'disabled'
+                    }`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };
