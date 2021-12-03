9babe7567f2e135bf0a7f418ca794c1aacafe7fb
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-03 19:10:22 +0330

    Add swiper slider to RecentProjects section component

--- components/section/RecentProjects.tsx
+++ components/section/RecentProjects.tsx
@@ -1,14 +1,80 @@
 // react
 import * as React from 'react';
+// swiper
+import { Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// @mui
+import { Card, CardContent, Typography } from '@mui/material';
 // type
 interface RecentProjectsProps {}
 
 const RecentProjects: React.FunctionComponent<RecentProjectsProps> = (
   props
 ) => {
+  const slides = [
+    {
+      title: 'slide',
+    },
+    {
+      title: 'slide',
+    },
+    {
+      title: 'slide',
+    },
+    {
+      title: 'slide',
+    },
+    {
+      title: 'slide',
+    },
+    {
+      title: 'slide',
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = React.useState<number>();
+
   return (
     <>
-      <p>Recent projects</p>
+      <Swiper
+        centeredSlides
+        spaceBetween={50}
+        slidesPerView={3}
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        onSwiper={(swiper) => setActiveIndex(swiper.activeIndex)}
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+        style={{
+          margin: '2rem',
+          paddingBottom: '2rem',
+          paddingTop: '2rem',
+          minHeight: '6rem',
+        }}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide
+            key={slide.title + index}
+            style={{
+              transform: index === activeIndex ? 'scale(1)' : 'scale(0.75)',
+              transition: '0.2s all ease-in-out',
+            }}
+          >
+            <Card>
+              <CardContent>
+                <Typography component="p" textAlign="center">
+                  {`${index} - ${
+                    index === activeIndex ? 'active' : 'disabled'
+                  }`}
+                </Typography>
+              </CardContent>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
