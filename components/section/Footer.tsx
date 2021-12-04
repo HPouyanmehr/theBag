594186d26ce8afce0300e3fe79b3268d15c70ccf
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-04 17:11:56 +0330

    Improve Footer section component

--- components/section/Footer.tsx
+++ components/section/Footer.tsx
@@ -1,25 +1,51 @@
 // react
 import * as React from 'react';
 // @mui
-import { Box, Typography, useTheme } from '@mui/material';
+import { Card, Typography, useTheme } from '@mui/material';
+// @mui icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+// custom component
 import ButtonLink from 'components/common/ButtonLink';
+import CustomIconButton from 'components/common/CustomIconButton';
 // type
 interface FooterProps {}
 
 const Footer: React.FunctionComponent<FooterProps> = (props) => {
-  const { palette } = useTheme();
+  const { palette, shape } = useTheme();
   return (
     <>
-      <Box
-        bgcolor={palette.primary.main}
-        padding="2rem"
-        margin="2rem"
-        borderRadius="4px"
+      <Card
+        style={{
+          backgroundColor: palette.primary.main,
+          padding: '2rem',
+          margin: '2rem',
+          borderRadius: shape.borderRadius,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
       >
-        <p>link</p>
-        <p>link</p>
-        <p>link</p>
-      </Box>
+        <div>
+          <ButtonLink sx={{ color: '#505050' }}>About</ButtonLink>
+          <ButtonLink sx={{ color: '#505050' }}>Skills</ButtonLink>
+          <ButtonLink sx={{ color: '#505050' }}>Certificates</ButtonLink>
+          <ButtonLink sx={{ color: '#505050' }}>Gallery</ButtonLink>
+        </div>
+        <div>
+          <CustomIconButton size="large" sx={{ color: '#505050' }}>
+            <FacebookIcon fontSize="inherit" />
+          </CustomIconButton>
+          <CustomIconButton size="large" sx={{ color: '#505050' }}>
+            <InstagramIcon fontSize="inherit" />
+          </CustomIconButton>
+          <CustomIconButton size="large" sx={{ color: '#505050' }}>
+            <TwitterIcon fontSize="inherit" />
+          </CustomIconButton>
+        </div>
+      </Card>
       <Typography component="p" variant="body1" textAlign="center">
         Made with ❤️ at{' '}
         <ButtonLink href="https://mopeim.com">mopeim</ButtonLink>
