78b4ebff824cc409f7eb9d6a2456a87a643977a7
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-05 13:58:09 +0330

    Make Footer section responsive

--- components/section/Footer.tsx
+++ components/section/Footer.tsx
@@ -1,15 +1,53 @@
 // react
 import * as React from 'react';
 // @mui
-import { Card, CardContent, Typography } from '@mui/material';
+import {
+  Box,
+  BoxProps,
+  Card,
+  CardProps,
+  CardContent,
+  CardContentProps,
+  Typography,
+  styled,
+} from '@mui/material';
 // custom component
-import CustomIconButton from 'components/common/CustomIconButton';
 import TextLink from 'components/common/TextLink';
 import FacebookIconLink from 'components/common/FacebookIconLink';
 import InstagramIconLink from 'components/common/InstagramIconLink';
 import TwitterIconLink from 'components/common/TwitterIconLink';
 // type
 interface FooterProps {}
+
+const CustomCard = styled(Card)<CardProps>(({ theme }) => ({
+  margin: '1rem',
+  padding: '1rem',
+  [theme.breakpoints.up('sm')]: {
+    margin: '2rem',
+    padding: '2rem',
+  },
+}));
+
+const CustomCardContent = styled(CardContent)<CardContentProps>(
+  ({ theme }) => ({
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '0.5rem',
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row',
+    },
+    a: {
+      marginBottom: '1rem',
+      [theme.breakpoints.up('sm')]: {
+        marginBottom: 0,
+        marginLeft: '0.5rem',
+        marginRight: '0.5rem',
+      },
+    },
+  })
+);
 
 const Footer: React.FunctionComponent<FooterProps> = (props) => {
   const navigationLinks = [
@@ -21,36 +59,20 @@ const Footer: React.FunctionComponent<FooterProps> = (props) => {
 
   return (
     <>
-      <Card
-        style={{
-          padding: '2rem',
-          margin: '2rem',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <CardContent style={{ padding: '0.5rem' }}>
+      <CustomCard>
+        <CustomCardContent>
           {navigationLinks.map((navigation, index) => (
-            <TextLink
-              key={navigation.title + index}
-              href={navigation.href}
-              style={{
-                marginLeft: '0.5rem',
-                marginRight: '0.5rem',
-              }}
-            >
+            <TextLink key={navigation.title + index} href={navigation.href}>
               {navigation.title}
             </TextLink>
           ))}
-        </CardContent>
-        <CardContent style={{ padding: '0.5rem' }}>
+        </CustomCardContent>
+        <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
           <FacebookIconLink />
           <InstagramIconLink />
           <TwitterIconLink />
         </CardContent>
-      </Card>
+      </CustomCard>
       <Typography component="p" variant="body1" textAlign="center">
         Made with ❤️ at <TextLink href="https://mopeim.com">mopeim</TextLink>
       </Typography>
