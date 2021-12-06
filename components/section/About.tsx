25e3fbc3cfc092fb1526da6ba15160b03729041a
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-06 12:28:13 +0330

    Make About section component responsive

--- components/section/About.tsx
+++ components/section/About.tsx
@@ -3,39 +3,68 @@ import * as React from 'react';
 // next
 import Image from 'next/image';
 // @mui
-import { Card, Grid, Typography, useTheme } from '@mui/material';
-// @mui icons
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import {
+  Box,
+  BoxProps,
+  Card,
+  CardActions,
+  CardActionsProps,
+  CardProps,
+  Grid,
+  Typography,
+  styled,
+} from '@mui/material';
 // custom component
-import CustomIconButton from 'components/common/CustomIconButton';
+import FacebookIconLink from 'components/common/FacebookIconLink';
+import InstagramIconLink from 'components/common/InstagramIconLink';
+import TwitterIconLink from 'components/common/TwitterIconLink';
+import ContainerGrid from 'components/common/ContainerGrid';
 // type
 interface AboutProps {}
 
-const About: React.FunctionComponent<AboutProps> = (props) => {
-  const {
-    palette: { info, primary },
-  } = useTheme();
+const ImageWrapper = styled(Box)<BoxProps>(({ theme }) => ({
+  position: 'relative',
+  width: '100%',
+  height: '21rem',
+  [theme.breakpoints.up(350)]: {
+    height: '26rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    height: '50vh',
+  },
+  [theme.breakpoints.up('md')]: {
+    height: '100%',
+  },
+}));
 
+const CustomCard = styled(Card)<CardProps>(({ theme }) => ({
+  overflow: 'hidden',
+  margin: '1rem',
+  [theme.breakpoints.up('md')]: {
+    height: 'fit-content',
+  },
+  [theme.breakpoints.up('lg')]: {
+    height: '90vh',
+  },
+}));
+
+const CustomCardActions = styled(CardActions)<CardActionsProps>(
+  ({ theme }) => ({
+    width: '100%',
+    justifyContent: 'flex-end',
+    [theme.breakpoints.up('lg')]: {
+      position: 'absolute',
+      bottom: 0,
+    },
+  })
+);
+
+const About: React.FunctionComponent<AboutProps> = (props) => {
   return (
-    <Card
-      style={{
-        height: '90vh',
-        margin: '2rem',
-        overflow: 'hidden',
-      }}
-      id="about"
-    >
-      <Grid container spacing={2} sx={{ marginTop: 0, height: '100%' }}>
+    <CustomCard id="about">
+      <ContainerGrid sx={{ marginTop: 0, height: '100%' }}>
         <Grid item xs={12} md={6} style={{ padding: 0 }}>
-          <div
-            style={{
-              height: '100%',
-              position: 'relative',
-              width: '100%',
-            }}
-          >
+          <ImageWrapper>
             <Image
               alt="John Doe profile image"
               layout="fill"
@@ -44,7 +73,7 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
               objectPosition="center center"
               src="/profile.jpg"
             />
-          </div>
+          </ImageWrapper>
         </Grid>
         <Grid
           item
@@ -52,49 +81,41 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
           md={6}
           style={{ padding: '2rem', position: 'relative' }}
         >
-          <Typography component="h2" variant="h5" fontWeight="bold">
-            A bit about me
-          </Typography>
-          <Typography component="p" variant="body1">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Aspernatur, sed nobis. Facere est eaque at blanditiis vel dolore
-            ipsam ad nam? Sed saepe commodi ducimus quae odio vero assumenda
-            reprehenderit reiciendis optio maiores perspiciatis doloribus,
-            praesentium non accusantium, beatae perferendis molestias!
-            Asperiores non praesentium, ex suscipit officia ad impedit nulla
-            laboriosam magnam doloremque perspiciatis alias esse corrupti beatae
-            dolore perferendis, blanditiis illo adipisci dolores modi eius
-            minus. Qui, sunt. Ipsum officiis aliquid pariatur iusto deleniti.
-          </Typography>
-          <Typography component="p" variant="body1">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
-            illo, architecto officiis recusandae praesentium impedit pariatur
-            repellendus, natus ducimus sapiente eius odio aspernatur magni
-            repellat explicabo laboriosam voluptatibus excepturi voluptas,
-            accusamus libero vel tempora quod consequuntur. Quaerat id eveniet
-            eaque?
-          </Typography>
-          <div
-            style={{
-              position: 'absolute',
-              padding: '2rem',
-              bottom: 0,
-              right: 0,
-            }}
+          <Card
+            sx={{ boxShadow: 'none', height: '100%', position: 'relative' }}
           >
-            <CustomIconButton color="inherit">
-              <FacebookIcon />
-            </CustomIconButton>
-            <CustomIconButton color="inherit">
-              <InstagramIcon />
-            </CustomIconButton>
-            <CustomIconButton color="inherit">
-              <TwitterIcon />
-            </CustomIconButton>
-          </div>
+            <Typography component="h2" variant="h5" fontWeight="bold">
+              A bit about me
+            </Typography>
+            <Typography component="p" variant="body1">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur, sed nobis. Facere est eaque at blanditiis vel dolore
+              ipsam ad nam? Sed saepe commodi ducimus quae odio vero assumenda
+              reprehenderit reiciendis optio maiores perspiciatis doloribus,
+              praesentium non accusantium, beatae perferendis molestias!
+              Asperiores non praesentium, ex suscipit officia ad impedit nulla
+              laboriosam magnam doloremque perspiciatis alias esse corrupti
+              beatae dolore perferendis, blanditiis illo adipisci dolores modi
+              eius minus. Qui, sunt. Ipsum officiis aliquid pariatur iusto
+              deleniti.
+            </Typography>
+            <Typography component="p" variant="body1">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+              illo, architecto officiis recusandae praesentium impedit pariatur
+              repellendus, natus ducimus sapiente eius odio aspernatur magni
+              repellat explicabo laboriosam voluptatibus excepturi voluptas,
+              accusamus libero vel tempora quod consequuntur. Quaerat id eveniet
+              eaque?
+            </Typography>
+            <CustomCardActions>
+              <FacebookIconLink />
+              <InstagramIconLink />
+              <TwitterIconLink />
+            </CustomCardActions>
+          </Card>
         </Grid>
-      </Grid>
-    </Card>
+      </ContainerGrid>
+    </CustomCard>
   );
 };
 
