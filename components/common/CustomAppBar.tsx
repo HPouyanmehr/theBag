a88aefa95865a90ff62ccdb1f6b785fbab31b506
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-07 12:55:06 +0330

    Add dropdown menu for small devices

--- components/common/CustomAppBar.tsx
+++ components/common/CustomAppBar.tsx
@@ -7,10 +7,14 @@ import {
   Box,
   BoxProps,
   Container,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Slide,
   styled,
   Toolbar,
 } from '@mui/material';
-
 // custom component
 import NameLogo from 'components/common/NameLogo';
 import ButtonLink from 'components/common/ButtonLink';
@@ -39,8 +43,10 @@ const LinksBox = styled(Box)<BoxProps>(({ theme }) => ({
 }));
 
 const CustomAppBar: React.FunctionComponent<CustomAppBarProps> = (props) => {
+  const [openDropdown, setOpenDropdown] = React.useState(false);
   const { navLinks } = React.useContext(ConstantsContext);
   navLinks?.sort((a, b) => (a.order > b.order ? 1 : -1));
+  const containerRef = React.useRef(null);
 
   return (
     <>
@@ -60,9 +66,41 @@ const CustomAppBar: React.FunctionComponent<CustomAppBarProps> = (props) => {
                   </ButtonLink>
                 ))}
               </LinksBox>
-              <MenuToggler color="info" sx={{ display: { sm: 'none' } }} />
+              <MenuToggler
+                color="info"
+                onClick={() => setOpenDropdown(!openDropdown)}
+                sx={{ display: { sm: 'none' } }}
+              />
             </LinkContainer>
           </Toolbar>
+          <Box ref={containerRef}>
+            <Slide
+              container={containerRef.current}
+              direction="down"
+              in={openDropdown}
+              mountOnEnter
+              unmountOnExit
+            >
+              <List component="div">
+                {navLinks?.map((navLink, index) => (
+                  <ListItemButton
+                    key={navLink.label + index}
+                    sx={{
+                      '&: hover': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                        borderRadius: '4px',
+                      },
+                    }}
+                  >
+                    <ListItemIcon {...navLink.iconProps}>
+                      {navLink.Icon}
+                    </ListItemIcon>
+                    <ListItemText primary={navLink.label} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </Slide>
+          </Box>
         </Container>
       </CustomAppBarRoot>
     </>
