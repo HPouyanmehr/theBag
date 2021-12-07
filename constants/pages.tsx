2b301e1db8e968b432ea72993ddbb081ad9edb5a
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-07 19:16:50 +0330

    Add projects page data to pages constant

--- constants/pages.tsx
+++ constants/pages.tsx
@@ -14,6 +14,11 @@ const pages: Pages = {
     description:
       'theBag is a multipurpose template made by Nextjs and MUI v5 which can be used for Portfolios, Resumes, CVs, and Personal websites.',
   },
+  projects: {
+    title: 'Projects',
+    description:
+      'You can see all my projects as a designer and photographer in this page.',
+  },
 };
 
 export default pages;
