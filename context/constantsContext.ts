bfe26b426cb0d2c998c46aa8ba4ab8a4400bada1
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-07 23:03:17 +0330

    Add projectsData to ConstantsContext

--- context/constantsContext.ts
+++ context/constantsContext.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 // data
 import navLinks from 'constants/navLinks';
 import pages from 'constants/pages';
+import projectsData, { ProjectDetails } from 'constants/projectsData';
 // type
 import { NavLink } from 'constants/navLinks';
 import { Pages } from 'constants/pages';
@@ -10,11 +11,13 @@ import { Pages } from 'constants/pages';
 interface ContextProps {
   navLinks?: NavLink[];
   pages?: Pages;
+  projects?: ProjectDetails[];
 }
 
 const context: ContextProps = {
   navLinks,
   pages,
+  projects: projectsData,
 };
 
 const ConstantsContext = React.createContext(context);
