72ec297ba5bea3d63d163bc70adf3e27a6e6991d
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-09 21:39:05 +0330

    Refactor CustomAnchor to make it cleaner

--- components/common/CustomAnchor.tsx
+++ components/common/CustomAnchor.tsx
@@ -6,30 +6,30 @@ import styled from '@emotion/styled';
 interface CustomAnchorProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
 
+const Root = styled('a')({
+  position: 'relative',
+  color: 'inherit',
+  textDecoration: 'none',
+  cursor: 'pointer',
+  '&:before': {
+    backgroundColor: 'currentcolor',
+    bottom: 0,
+    content: '""',
+    display: 'block',
+    height: '2px',
+    left: 0,
+    position: 'absolute',
+    transform: 'scaleX(0)',
+    transition: 'transform 0.3s ease',
+    width: '100%',
+  },
+  '&:hover:before': {
+    transform: 'scaleX(1)',
+  },
+});
+
 const CustomAnchor: React.FunctionComponent<CustomAnchorProps> = (props) => {
   const { children, ...otherProps } = props;
-
-  const Root = styled('a')({
-    position: 'relative',
-    color: 'inherit',
-    textDecoration: 'none',
-    cursor: 'pointer',
-    '&:before': {
-      backgroundColor: 'currentcolor',
-      bottom: 0,
-      content: '""',
-      display: 'block',
-      height: '2px',
-      left: 0,
-      position: 'absolute',
-      transform: 'scaleX(0)',
-      transition: 'transform 0.3s ease',
-      width: '100%',
-    },
-    '&:hover:before': {
-      transform: 'scaleX(1)',
-    },
-  });
 
   return <Root {...otherProps}>{children}</Root>;
 };
