336ac57918476b57a907f615a3cfbcbb3246b7c7
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-09 17:35:19 +0330

    Add hire me page data in pages constants

--- constants/pages.tsx
+++ constants/pages.tsx
@@ -19,6 +19,11 @@ const pages: Pages = {
     description:
       'You can see all my projects as a designer and photographer in this page.',
   },
+  hireMe: {
+    title: 'Hire me',
+    description:
+      'I am available as freelancer to do your projects, fill the form to contact to me.',
+  },
 };
 
 export default pages;
