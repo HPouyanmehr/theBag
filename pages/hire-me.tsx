395446f6e8404ab60a4ff757b7f15f5f10c23977
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-11 13:11:31 +0330

    Improve hire-me page

--- pages/hire-me.tsx
+++ pages/hire-me.tsx
@@ -1,22 +1,69 @@
 // react
 import * as React from 'react';
+// @mui
+import { Box, BoxProps, Grid, styled, Typography } from '@mui/material';
 // custom component
+import ContactForm from 'components/common/ContactForm';
 import MainLayout from 'components/layout/MainLayout';
-import CenterBox from 'components/common/CenterBox';
 // custom context
 import ConstantsContext from 'context/constantsContext';
 // type
 import type { NextPage } from 'next';
-import ContactForm from 'components/common/ContactForm';
+import ContainerGrid from 'components/common/ContainerGrid';
+
+const CustomBox = styled(Box)<BoxProps>(({ theme }) => ({
+  margin: '30% 1rem 5% 1rem',
+  [theme.breakpoints.up(350)]: {
+    margin: '20% 1rem 5% 1rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    margin: '15% 2rem 5% 2rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    margin: '10% 2rem 5% 2rem',
+  },
+}));
 
 const HireMe: NextPage = (props) => {
   const { pages } = React.useContext(ConstantsContext);
 
   return (
     <MainLayout pageData={pages && pages.hireMe}>
-      <CenterBox>
-        <ContactForm />
-      </CenterBox>
+      <CustomBox>
+        <ContainerGrid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            alignItems="center"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Box
+              sx={{
+                width: {
+                  xs: '100%',
+                  md: '70%',
+                },
+              }}
+            >
+              <Typography component="h2" variant="h4">
+                What can I do for you?
+              </Typography>
+              <Typography color="text.secondary" component="p" variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+                laborum similique voluptas pariatur a doloremque facilis minima
+                quam dolores eveniet eaque aspernatur modi hic? Quod sunt in
+                recusandae adipisci possimus?
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <ContactForm />
+          </Grid>
+        </ContainerGrid>
+      </CustomBox>
     </MainLayout>
   );
 };
