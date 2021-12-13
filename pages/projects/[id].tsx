77ce19448d37d59e09b8580f3e477b77e0bd60bb
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-13 15:43:30 +0330

    change md to mdx

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,7 +1,13 @@
+// fs
+import fs from 'fs';
+// path
+import path from 'path';
 // react
 import * as React from 'react';
-// next
-import { useRouter } from 'next/router';
+// gray-matter
+import { remark } from 'remark';
+// markdown-to-jsx
+import Markdown from 'markdown-to-jsx';
 // @mui
 import { Box, BoxProps, Grid, styled } from '@mui/material';
 // custom component
@@ -9,11 +15,21 @@ import ContainerGrid from 'components/common/ContainerGrid';
 import Gallery from 'components/common/Gallery';
 import MainLayout from 'components/layout/MainLayout';
 import ProjectDetails from 'components/section/ProjectDetails';
-// custom context
-import ConstantsContext from 'context/constantsContext';
+// projects data
+import projectsData, { Project } from 'constants/projectsData';
 // type
-import type { NextPage } from 'next';
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  NextPage,
+} from 'next';
 import ProjectNotFound from 'components/section/ProjectNotFound';
+
+interface ProjectPageProps {
+  project?: string;
+  content?: string;
+}
 
 const ProjectContainer = styled(Box)<BoxProps>(({ theme }) => ({
   margin: '5rem 1rem 1rem',
@@ -22,10 +38,8 @@ const ProjectContainer = styled(Box)<BoxProps>(({ theme }) => ({
   },
 }));
 
-const Project: NextPage = () => {
-  const { projects } = React.useContext(ConstantsContext);
-  const router = useRouter();
-  const { id } = router.query;
+const ProjectPage: NextPage<ProjectPageProps> = (props) => {
+  const { project, content = 'no content' } = props;
 
   const NotFound = (
     <MainLayout pageData={{ title: 'Project Not Found' }}>
@@ -33,21 +47,21 @@ const Project: NextPage = () => {
     </MainLayout>
   );
 
-  if (!projects) return NotFound;
-
-  const project = projects.find((project) => project.id === id);
   if (!project) return NotFound;
+
+  const parsedProject: Project = JSON.parse(project);
 
   return (
     <>
-      <MainLayout pageData={{ title: project.title }}>
+      <MainLayout pageData={{ title: parsedProject.title }}>
         <ProjectContainer>
           <ContainerGrid>
             <Grid item xs={12} md={8}>
-              <Gallery images={project.images} />
+              <Gallery images={parsedProject.images} />
+              <Markdown children={content} />
             </Grid>
             <Grid item xs>
-              <ProjectDetails project={project} />
+              {/* <ProjectDetails project={project} /> */}
             </Grid>
           </ContainerGrid>
         </ProjectContainer>
@@ -56,4 +70,31 @@ const Project: NextPage = () => {
   );
 };
 
-export default Project;
+export default ProjectPage;
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const { params } = context;
+  const project = projectsData.find((project) => project.id === params!.id);
+
+  if (!project) return { props: {} };
+
+  const filename = project.postFileName + '.md';
+  const md = fs.readFileSync(path.join(`posts\\${filename}`));
+  const { value: content } = remark.processSync(md);
+
+  return {
+    props: {
+      project: JSON.stringify(project),
+      content,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
