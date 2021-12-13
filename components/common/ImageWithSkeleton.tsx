6b5491e0a4e0993ede0596a744c6960a8f1f41ee
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-13 18:53:15 +0330

    Add custom component to render nextjs Image with Skeleton

---
+++ components/common/ImageWithSkeleton.tsx
@@ -0,0 +1,31 @@
+// react
+import * as React from 'react';
+// next
+import Image, { ImageProps } from 'next/image';
+import { Skeleton, useTheme } from '@mui/material';
+
+const ImageWithSkeleton: React.FunctionComponent<ImageProps> = (props) => {
+  const { children, onLoad, ...otherProps } = props;
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const {
+    palette: { primary },
+  } = useTheme();
+
+  return (
+    <>
+      <Image onLoad={() => setIsLoaded(true)} {...otherProps} />
+      {!isLoaded && (
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            backgroundColor: primary.light,
+            borderRadius: '4px',
+            height: '100%',
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default ImageWithSkeleton;
