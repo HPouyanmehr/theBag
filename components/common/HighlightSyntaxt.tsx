8ecdad5cd3548cc8fec4ed881f4963bba9abe77c
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-14 17:09:04 +0330

    Add custom syntax highlighter component

---
+++ components/common/HighlightSyntaxt.tsx
@@ -0,0 +1,44 @@
+// react
+import * as React from 'react';
+// @mui
+import { styled } from '@mui/material';
+// prism-react-renderer
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import vsDark from 'prism-react-renderer/themes/vsDark';
+// type
+interface HighlightSyntaxtProps {
+  code?: string;
+  language?: Language;
+}
+
+const Pre = styled('pre')<React.HTMLAttributes<HTMLPreElement>>({
+  padding: '1rem',
+  borderRadius: '4px',
+});
+
+const HighlightSyntaxt: React.FunctionComponent<HighlightSyntaxtProps> = (
+  props
+) => {
+  const {
+    code = `const sayHello = (name:string) => console.log(\`Hello\${name}\`);`,
+    language = 'tsx',
+  } = props;
+
+  return (
+    <Highlight {...defaultProps} theme={vsDark} code={code} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default HighlightSyntaxt;
