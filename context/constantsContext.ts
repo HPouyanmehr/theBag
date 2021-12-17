24c8ac15ea1c283ec94276e9d814880bd6538227
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-17 21:18:44 +0330

    Add blog posts to the constantsContext

--- context/constantsContext.ts
+++ context/constantsContext.ts
@@ -4,20 +4,24 @@ import * as React from 'react';
 import navLinks from 'constants/navLinks';
 import pages from 'constants/pages';
 import projectsData, { Project } from 'constants/projectsData';
+import blogPosts from 'constants/blogPostsData';
 // type
 import { NavLink } from 'constants/navLinks';
 import { Pages } from 'constants/pages';
+import { BlogPost } from 'types/blogPostType';
 
 interface ContextProps {
   navLinks?: NavLink[];
   pages?: Pages;
   projects?: Project[];
+  blogPosts?: BlogPost[];
 }
 
 const context: ContextProps = {
   navLinks,
   pages,
   projects: projectsData,
+  blogPosts,
 };
 
 const ConstantsContext = React.createContext(context);
