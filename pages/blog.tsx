9ec976f7a08300c44af0f48a1aa237a08f4c9cdb
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-17 21:30:56 +0330

    Update blog posts page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,22 +1,45 @@
 // react
 import * as React from 'react';
+// @mui
+import { Grid, Typography } from '@mui/material';
 // custom component
+import BlogCard from 'components/common/BlogCard';
+import ContainerGrid from 'components/common/ContainerGrid';
 import MainLayout from 'components/layout/MainLayout';
 // custom context
 import CenterBox from 'components/common/CenterBox';
 import ConstantsContext from 'context/constantsContext';
 // type
 import type { NextPage } from 'next';
-import BlogCard from 'components/common/BlogCard';
 
 const Blog: NextPage = (props) => {
-  const { pages } = React.useContext(ConstantsContext);
+  const { pages, blogPosts } = React.useContext(ConstantsContext);
 
   return (
     <MainLayout pageData={pages && pages.projects}>
-      <CenterBox>
-        <BlogCard />
-      </CenterBox>
+      {blogPosts ? (
+        <ContainerGrid
+          sx={{ padding: { xs: '1rem', sm: '2rem' }, marginTop: '5rem' }}
+        >
+          {blogPosts.map((post, index) => (
+            <Grid item key={post.title + index} xs={12} md={4} lg={3}>
+              <BlogCard
+                href={`/blog/${post.id}`}
+                readTime={5}
+                imageAlt={post.image.alt}
+                imageSrc={post.image.src}
+                title={post.title}
+              />
+            </Grid>
+          ))}
+        </ContainerGrid>
+      ) : (
+        <CenterBox>
+          <Typography component="p" variant="body1">
+            No post availabe.
+          </Typography>
+        </CenterBox>
+      )}
     </MainLayout>
   );
 };
