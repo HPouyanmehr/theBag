6945b1a7ebaf8e0efe53b6061a46a13c14061225
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-18 18:00:24 +0330

    Add getBlogPost logic

--- lib/blogPost.ts
+++ lib/blogPost.ts
@@ -1,5 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+import { remark } from 'remark';
 // type
 import { BlogPost, BlogPostDetails } from 'types/blogPostType';
+
+const blogPostsDir = path.join(process.cwd(), 'posts/blog');
 
 export const createPost = (props: BlogPostDetails): BlogPost => {
   const { category, date, id, image, filename, title } = props;
@@ -15,4 +20,49 @@ export const createPost = (props: BlogPostDetails): BlogPost => {
       return this.image.likes;
     },
   };
+};
+
+export const getBlogPostById = async (
+  id: string
+): Promise<{ content: string | null }> => {
+  const blogPostFullPath = path.join(blogPostsDir, `${id}.md`);
+
+  try {
+    const fileContent = fs.readFileSync(blogPostFullPath, 'utf8');
+
+    const processedContent = await remark().process(fileContent);
+    const content = processedContent.toString();
+
+    return {
+      content,
+    };
+  } catch (error) {
+    console.error(`error - Couldn't find ${id}.mdx file to read.`);
+  }
+
+  return {
+    content: null,
+  };
+};
+
+export const getBlogPostsId = () => {
+  try {
+    const fileNames = fs.readdirSync(blogPostsDir);
+
+    return fileNames.map((fileName) => ({
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    }));
+  } catch (error) {
+    console.log(`error - Couldn't read this dir files: ${blogPostsDir}`);
+  }
+
+  return [
+    {
+      params: {
+        id: '',
+      },
+    },
+  ];
 };
