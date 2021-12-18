66028cefdfe4802667cec42b7f40c25bca547dd8
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-18 19:04:03 +0330

    Add fetching post logic to blog dynamic post page

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,112 +1,108 @@
 // react
-import { Box, BoxProps, Typography, styled } from '@mui/material';
+import * as React from 'react';
+// dayjs
+import dayjs from 'dayjs';
+// reading-time
+import readingTime from 'reading-time';
+// @mui
+import { Box, BoxProps, Grid, Typography, styled } from '@mui/material';
+// custom component
 import Markdown from 'components/common/Markdown';
 import MainLayout from 'components/layout/MainLayout';
-import * as React from 'react';
+// blogPost data
+import blogPosts from 'constants/blogPostsData';
+// custom lib
+import { getBlogPostsId, getBlogPostById } from 'lib/blogPost';
 // type
-interface BlogPostProps {}
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  NextPage,
+} from 'next';
+import { BlogPost as BlogPostDataProps } from 'types/blogPostType';
+import ProjectNotFound from 'components/section/ProjectNotFound';
+import ContainerGrid from 'components/common/ContainerGrid';
+interface BlogPostProps {
+  blogPost?: string;
+  content?: string;
+}
 
 const CustomContainer = styled(Box)<BoxProps>(({ theme }) => ({
-  margin: '5rem 1rem 1rem',
+  margin: '10rem 1rem 1rem',
   [theme.breakpoints.up('sm')]: {
-    margin: '5rem 2rem 1rem',
+    margin: '10rem 2rem 1rem',
   },
 }));
 
-const BlogPost: React.FunctionComponent<BlogPostProps> = (props) => {
+const BlogPost: NextPage<BlogPostProps> = (props) => {
+  const { blogPost, content = '' } = props;
+
+  if (!blogPost) return <ProjectNotFound />;
+
+  const parsedBlogPost: BlogPostDataProps = JSON.parse(blogPost);
+  const { minutes } = readingTime(content);
+  const readTime = Math.ceil(minutes);
+  const date = dayjs(parsedBlogPost.date);
+
   return (
     <MainLayout>
       <CustomContainer>
-        <Typography component="h1" textAlign="center" variant="h3">
-          Post title
+        <Typography component="h1" textAlign="center" variant="h4">
+          {parsedBlogPost.title}
         </Typography>
         <Typography
           color="text.secondary"
           component="p"
+          marginTop="1rem"
           textAlign="center"
           variant="body1"
         >
-          Read time - Published at date
+          {readTime} Read time - Published at {date.toDate().toDateString()}
         </Typography>
-        <Markdown
-          content={`# Heading level 1
-
-## Heading level 2
-
-### Heading level 3
-
-#### Heading level 4
-
-##### Heading level 5
-
-###### Heading level 6
-
-A **paragraph**: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, numquam ipsam. Quidem dolor beatae eius doloremque delectus deserunt veniam necessitatibus!
-
-I am a sentence containing  
-line break or <br>
-
-I use **bold text** here. (<strong></strong>)
-Italicized text is the _cat's meow_. (<em></em>)
-
-This text is **_bold and italic_**.
-
-> This is a blockquote
->
-> > This is a nested blockquote
-
-> #### This is combination of blockquote and other elements
->
-> - first item of a list.
-> - second item of a list.
->
->   _Italic_ or **bold**.
-
-### ordered list
-
-1. First item
-2. Second item
-3. Third item
-   1. Indented item
-   2. Indented item
-4. Fourth item
-
-### unordered list
-
-- First item
-- Second item
-- Third item
-- Fourth item
-
-### pre block
-
-<pre>Hi, I am haj hosein This is equal to pre block in html.</pre>
-
-### images
-
-![freelancer](/sections/freelancer-male.svg)
-
-### Horizontal Rule
-
----
-
-### Links
-
-This is a [mopeim](https://mopeim.com) template.
-
-### Sample Table
-
-| Column A | Column B | Column C |
-| -------- | -------- | -------- |
-| A1       | B1       | C1       |
-| A2       | B2       | C2       |
-| A3       | B3       | C3       |
-`}
-          sx={{ display: 'flex', justifyContent: 'center' }}
-        />
+        <ContainerGrid>
+          <Grid item xs={12} md={8} sx={{ margin: '3rem auto 1rem' }}>
+            <Markdown content={content} />
+          </Grid>
+        </ContainerGrid>
       </CustomContainer>
     </MainLayout>
   );
 };
 
 export default BlogPost;
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const { params } = context;
+  const blogPost = blogPosts.find((blogPost) => blogPost.id === params!.id);
+
+  if (!blogPost) return { props: {} };
+
+  const filename = blogPost.filename;
+  const { content } = await getBlogPostById(filename);
+
+  if (!content)
+    return {
+      props: {
+        blogPost: JSON.stringify(blogPost),
+      },
+    };
+
+  return {
+    props: {
+      blogPost: JSON.stringify(blogPost),
+      content,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getBlogPostsId();
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
